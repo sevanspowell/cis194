51819947c67df86782477274622f7c8853e4d955
Samuel Evans-Powell <sam@livebranchstudios.com.au>
2017-12-20 16:42:42 +1100

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,13 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Main where
+
+import Lib
+
+main :: IO ()
+main = someFunc
+
+-- Double the value of every second digit beginning from the right
+  -- Select every second digit
+  -- Double these
+-- Add the dig

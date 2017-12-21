825ad33f404a12711a887fb576c045886aebdd9b
Samuel Evans-Powell <sam@livebranchstudios.com.au>
2017-12-21 16:31:58 +1100

    HW 1 - Exc 1

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,22 @@
 module Main where
 
 import Lib
+import Prelude (IO, Integral, (++), (.), div, mod)
+
+import Data.List (reverse)
 
 main :: IO ()
 main = someFunc
 
--- Double the value of every second digit beginning from the right
-  -- Select every second digit
-  -- Double these
--- Add the dig
+--
+-- Exercise 1
+--
+
+-- Get the digits of a given integer
+toDigits :: Integral a => a -> [a]
+toDigits 0 = []
+toDigits x = toDigits (x `div` 10) ++ [x `mod` 10]
+
+-- Decompose a given integer into a list of it's digits, reversed.
+toDigitsRev :: Integral a => a -> [a]
+toDigitsRev = reverse . toDigits

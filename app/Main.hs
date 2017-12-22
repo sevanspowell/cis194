40d62b9d2c4d7bee36c027a59d40f9cf1826fcce
Samuel Evans-Powell <sam@livebranchstudios.com.au>
2017-12-22 14:44:58 +1100

    HW 01 - Exc 05

--- app/Main.hs
+++ app/Main.hs
@@ -65,4 +65,4 @@ type Move = (Peg, Peg)
 -- the second
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
-hanoi n a b c = hanoi (n - 1) a b c ++ [(a, b)] ++ hanoi (n - 1) c a b
+hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a

c108d4e8758cdd1498c5a2c3dc961a5279259133
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2018-01-04 00:36:39 +0800

    Homework 2 - Exc 5

--- src/Homework2/LogAnalysis.hs
+++ src/Homework2/LogAnalysis.hs
@@ -1,27 +1,67 @@
+{-# OPTIONS_GHC -Wall #-}
 
 module Homework2.LogAnalysis where
 
-import Homework2.Log
+import           Homework2.Log
 
+-- Produce a LogMessage from a log line.
 parseMessage :: String -> LogMessage
-parseMessage = parseLogMessage . words 
+parseMessage = parseLogMessage . words
   where
     parseLogMessage :: [String] -> LogMessage
-    parseLogMessage arr@(messageType : rest) =
-      case messageType of
-        "I" -> parseInfoMessage    rest
-        "W" -> parseWarningMessage rest
-        "E" -> parseErrorMessage   rest
-        _   -> Unknown (unwords arr)
+    parseLogMessage ("I" : timestamp : msg) =
+      LogMessage Info (read timestamp) (unwords msg)
+    parseLogMessage ("W" : timestamp : msg) =
+      LogMessage Warning (read timestamp) (unwords msg)
+    parseLogMessage ("E" : level : timestamp : msg) =
+      LogMessage (Error $ read level) (read timestamp) (unwords msg)
+    parseLogMessage arr =
+      Unknown (unwords arr)
 
-    parseInfoMessage :: [String] -> LogMessage
-    parseInfoMessage (timestamp : rest) = LogMessage Info (read timestamp) (unwords rest)
-
-    parseWarningMessage :: [String] -> LogMessage
-    parseWarningMessage (timestamp : rest) = LogMessage Warning (read timestamp) (unwords rest)
-
-    parseErrorMessage :: [String] -> LogMessage
-    parseErrorMessage (level : timestamp : rest) = LogMessage (Error $ read level) (read timestamp) (unwords rest)
-
+-- Produce an array of log messages from a log file.
 parse :: String -> [LogMessage]
 parse = map parseMessage . lines
+
+-- Given a log message and a sorted message tree, return a sorted message tree
+-- containing the given log message.
+insert :: LogMessage -> MessageTree -> MessageTree
+
+-- Return original tree unchanged if unknown log message
+insert (Unknown _) tree = tree
+
+-- Add message to tree
+insert msg Leaf = Node Leaf msg Leaf
+insert msg@(LogMessage _ insertTime _)
+       (Node left value@(LogMessage _ nodeTime _) right)
+  | insertTime < nodeTime = Node (insert msg left) value right
+  | insertTime > nodeTime = Node left value (insert msg right)
+  | otherwise             = Node left msg right
+
+-- Do nothing if found "unknown" log message in tree, tree should not contain
+-- these values
+insert (LogMessage _ _ _) tree@(Node _ (Unknown _) _) = tree
+
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (msg1 : rest) = insert msg1 (build rest)
+
+-- Given a sorted message tree, provides a list of all messages it contains,
+-- sorted by timestamp ascending
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left msg right) = (inOrder left) ++ [msg] ++ (inOrder right)
+
+-- Given an unsorted list of LogMessages, returns a list of messages
+-- corresponding to errors with a severity of 50 or greater
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = extractMsg . inOrder . build . filter (severity 50)
+  where 
+    severity :: Int -> LogMessage -> Bool
+    severity n (LogMessage (Error level) _ _) = level >= n
+    severity _ _ = False
+
+    extractMsg :: [LogMessage] -> [String]
+    extractMsg [] = []
+    extractMsg ((LogMessage _ _ msg) : rest) = [msg] ++ extractMsg rest
+    extractMsg ((Unknown msg) : rest) = [msg] ++ extractMsg rest

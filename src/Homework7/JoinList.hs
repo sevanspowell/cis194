3ae6721f17ad5c813f442ec59047c7ca9fc2b89b
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2018-01-11 14:20:21 +1100

    Finish Homework 7 - Exercise 2

--- src/Homework7/JoinList.hs
+++ src/Homework7/JoinList.hs
@@ -4,6 +4,7 @@
 module Homework7.JoinList where
 
 import Homework7.Sized
+import Data.Monoid
 
 -- Exercise 1
 
@@ -25,11 +26,6 @@ tag (Empty)        = mempty
 tag (Single m _)   = m
 tag (Append m _ _) = m
 
-getL :: Monoid m => JoinList m a -> JoinList m a
-getL (Empty)        = Empty
-getL a@(Single _ _)   = a
-getL (Append _ l _) = l 
-
 -- | Given two JoinLists, append them to form a new JoinList, whose monoidal
 --   annotation is derived from the two given JoinLists.
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
@@ -39,6 +35,13 @@ getL (Append _ l _) = l
 
 -- Exercise 2
 
+-- Example tree
+-- let t = (Append (Size 4) (Append (Size 3) (Single (Size 1) 'y') (Append (Size 2) (Single (Size 1) 'e') (Single (Size 1) 'a'))) (Single (Size 1) 'h'))
+sizeAnnot :: (Sized b) => JoinList b a -> Int
+sizeAnnot Empty = 0
+sizeAnnot (Single b _) = getSize $ size b
+sizeAnnot (Append b _ _) = getSize $ size b
+
 -- | Find the element of the JoinList at the given index. THe JoinList should
 --   use it's monoidal annotation to record the size of (number of elements in)
 --   each subtree.
@@ -46,11 +49,39 @@ indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty = Nothing
 indexJ 0 (Single _ a) = Just a
 indexJ _ (Single _ _) = Nothing
-indexJ i (Append m l1 l2)
-  | i < 0                       = Nothing
-  | i >= getSize (size m)       = Nothing
-  | i < getSize (size $ tag l1) = indexJ i l1
-  | otherwise                   = indexJ (i - (getSize (size $ tag l1))) l2
+indexJ i curr@(Append _ l1 l2)
+  | i < 0         = Nothing
+  | i >= sizeCurr = Nothing
+  | i < sizeL1    = indexJ i l1
+  | otherwise     = indexJ (i - sizeL1) l2
+  where
+    sizeCurr = sizeAnnot curr
+    sizeL1   = sizeAnnot l1
 
--- Example tree
--- let t = (Append (Size 4) (Append (Size 3) (Single (Size 1) 'y') (Append (Size 2) (Single (Size 1) 'e') (Single (Size 1) 'a'))) (Single (Size 1) 'h'))
+-- | Drop the first n elements from a JoinList.
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ n a | n <= 0 = a
+dropJ _ Empty = Empty
+dropJ _ (Single _ _) = Empty
+dropJ n curr@(Append _ l1 l2)
+  | n >= sizeCurr = Empty
+  | n >= sizeL1   = dropJ (n - sizeL1) l2
+  | otherwise     = Append (tag newL1 <> tag l2) newL1 l2
+  where
+    sizeCurr = sizeAnnot curr
+    sizeL1 = sizeAnnot l1
+    newL1  = dropJ n l1
+
+-- | Return the first n elements of a JoinList, dropping all other elements.
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ n _ | n <= 0 = Empty
+takeJ _ Empty = Empty
+takeJ _ a@(Single _ _) = a
+takeJ n curr@(Append _ l1 l2)
+  | n >= sizeCurr = curr
+  | n >  sizeL1   = Append (tag l1 <> tag newL2) l1 newL2
+  | otherwise     = (takeJ n l1)
+  where
+    sizeCurr = sizeAnnot curr
+    sizeL1   = sizeAnnot l1 
+    newL2    = takeJ (n - sizeAnnot l1) l2

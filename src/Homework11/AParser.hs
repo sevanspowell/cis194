f21f6264e4c14202ddb9e26326b37e9b48675d78
Samuel Evans-Powell <samueldavid.ep@gmail.com>
2018-01-22 08:19:25 +1100

    WIP

--- src/Homework11/AParser.hs
+++ src/Homework11/AParser.hs
@@ -1,10 +1,7 @@
-{-# LANGUAGE InstanceSigs #-}
-{-# OPTIONS_GHC -Wall #-}
-
 module Homework11.AParser (Parser, runParser, satisfy, char, posInt) where
 
-import Control.Applicative
-import Data.Char
+import           Control.Applicative
+import           Data.Char
 
 newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 
@@ -27,64 +24,21 @@ posInt = Parser f
       | otherwise = Just (read ns, rest)
       where (ns, rest) = span isDigit xs
 
--- | Take a function which maps one parsing function to another, a Parser a, and
---   produce a Parser b. The Parser b runs Parser a, but also runs the given
---   function after running Parser a.
-inParser :: ((String -> Maybe (a, String)) -> (String -> Maybe (b, String))) -> Parser a -> Parser b
 inParser f = Parser . f . runParser
 
--- | Apply the given function to the first element of a pair only
-first :: (a -> b) -> (a, c) -> (b, c)
-first f (a, c) = (f a, c)
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (x,y) = (f x, y)
 
 instance Functor Parser where
-  fmap :: (a -> b) -> Parser a -> Parser b
   fmap = inParser . fmap . fmap . first
-  -- (a -> b)
-  -- first :: (a -> b) -> (a, c) -> (b, c)
-  -- fmap  :: ((a, c) -> (b, c)) -> f (a, c) -> f (b, c)
-  -- fmap' :: (f (a, c) -> f (b, c)) -> g f (a, c) -> g f (b, c)
-
-  -- To fit 'inParser', f = Maybe, g  = ((->) String), c = String, making fmap':
-
-  -- fmap' :: (Maybe (a, String) -> Maybe (b, String)) -> (String -> Maybe (a, String)) -> (String -> Maybe (a, String))
-  -- fmap  :: ((a, String) -> (b, String)) -> Maybe (a, String) -> Maybe (b, String)
-  -- first :: (a -> b) -> (a, String) -> (b, String)
-
-  -- Which makes sense :)
 
 instance Applicative Parser where
-  pure :: a -> Parser a
-  pure x = Parser (\str -> Just (x, str))
+  pure a = Parser (\s -> Just (a, s))
+  (Parser fp) <*> xp = Parser $ \s ->
+    case fp s of
+      Nothing     -> Nothing
+      Just (f,s') -> runParser (f <$> xp) s'
 
-  (<*>) :: Parser (a -> b) -> Parser a -> Parser b
-  (Parser p1f) <*> p2 = Parser $ \s ->
-    case p1f s of
-      Nothing      -> Nothing
-      Just (f, s') -> runParser (f <$> p2) s' -- f :: (Parser a -> Parser b)
-  -- p1f :: String -> Maybe ((a -> b), String)
-  -- p1f :: Maybe ((a -> b), String)
-  -- f <$> p2 :: Parser b
-
-
-(*>) :: Applicative f => f a -> f b -> f b
--- (*>) a b = flip const <$> a <*> b
-(*>) = liftA2 (flip const)
-
--- mapA :: Applicative f => (a -> f b) -> ([a] -> f [b])
--- mapA f = (\xs -> (pure (map f)) <*> (pure xs))
--- Lift xs into context of Applicative Functor
--- Lift 'map f' into context of Applicative Functor
--- Apply Applicative f to Applicative list
-
--- (<*>) :: f ([a] -> [b]) -> f ([a]) -> f ([b])
-
-mySequenceA :: Applicative f => [f a] -> f [a]
-mySequenceA [] = pure []
-mySequenceA (x:xs) = (:) <$> x <*> mySequenceA xs
-
-mapA :: (a -> Maybe b) -> ([a] -> Maybe [b])
-mapA f = mySequenceA . map f
-
-replicateA :: Applicative f => Int -> f a -> f [a]
-replicateA n f = fmap (replicate n) f
+instance Alternative Parser where
+  empty = Parser (const Nothing)
+  Parser p1 <|> Parser p2 = Parser $ liftA2 (<|>) p1 p2
--- src/Homework11/SExpr.hs
+++ src/Homework11/SExpr.hs
@@ -2,20 +2,20 @@
    due Monday, 8 April
 -}
 
-module SExpr where
+module Homework11.SExpr where
 
-import AParser
+import Homework11.AParser
 import Control.Applicative
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
-zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+-- zeroOrMore :: Parser a -> Parser [a]
+-- zeroOrMore p = undefined
 
-oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+-- oneOrMore :: Parser a -> Parser [a]
+-- oneOrMore p = undefined
 
 ------------------------------------------------------------
 --  2. Utilities
@@ -44,3 +44,39 @@ data Atom = N Integer | I Ident
 data SExpr = A Atom
            | Comb [SExpr]
   deriving Show
+
+(*>) :: Applicative f => f a -> f b -> f b
+-- (*>) a b = flip const <$> a <*> b
+(*>) = liftA2 (flip const)
+
+mySequenceA :: Applicative f => [f a] -> f [a]
+mySequenceA [] = pure []
+mySequenceA (x:xs) = (:) <$> x <*> mySequenceA xs
+
+mapA :: (a -> Maybe b) -> ([a] -> Maybe [b])
+mapA f = mySequenceA . map f
+
+replicateA :: Applicative f => Int -> f a -> f [a]
+replicateA n f = fmap (replicate n) f
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = ((:) <$> p <*> zeroOrMore p) <|> ((\_ -> []) <$> p)
+-- zeroOrMore p = (++) <$> (((:) []) <$> p) <*> (((:) []) <$> p)
+  -- where
+  --   go = Parser $ (\str -> case runParser p str of
+  --                             Nothing -> Just ([], str)
+  --                             Just (a, str') -> Just ([a], str'))
+
+-- zeroOrMore p = go
+--   where
+--     go = Parser $ (\str -> case runParser p str of
+--                               Nothing -> Just ([], str)
+--                               Just (a, str') -> (\(a1, _) (a2, s2) -> (a1 ++ a2, s2)) <$> Just ([a], str') <*> runParser (zeroOrMore p) str')
+
+
+-- oneOrMore :: Parser a -> Parser [a]
+-- oneOrMore p = go
+--   where
+--     go = Parser $ (\str -> case runParser p str of
+--                               Nothing -> Nothing
+--                               Just (a, str') -> (\(a1, _) (a2, s2) -> (a1 ++ a2, s2)) <$> Just ([a], str') <*> runParser (zeroOrMore p) str')
